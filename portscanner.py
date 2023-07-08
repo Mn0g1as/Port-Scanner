53d350dc3fde4fcee916cca01e3e812ea26bbfe5
GoNz0 | Mn0g1as <128939384+Mn0g1as@users.noreply.github.com>
2023-07-08 17:52:03 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,1 +1,17 @@
+#Made by Mn0g1as
+# Have fun :)
+
+import socket
+ip =  'ip'
+#port 9000
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  for port in range(100, 65500):
+    try:
+      s.connect(ip, port)
+      print(f' port {port} is open! ')
+
+except:
+
+print(f' port {port} is not open! ')
+
 
